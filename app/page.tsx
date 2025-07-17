55670a0cc09aa49e09813eb6e496151140283fa8
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2025-07-17 10:15:58 +0100

    Making progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { BiPhoneCall, BiStar } from "react-icons/bi";
 import { useState, useEffect } from "react";
-import Marquee from "react-fast-marquee"
 import { BiMenu } from "react-icons/bi";
-
+import { BsArrowUpRight } from "react-icons/bs";
 export default function Home() {
   const [scrolled, setScrolled] = useState(false);
 
@@ -32,7 +31,7 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
 
-        <div className="absolute top-[28%] px-6 lg:px-0 lg:left-25 flex flex-col gap-8 lg:w-[50%]">
+        <div className="absolute top-[28%] px-6 lg:px-0 lg:left-25 flex flex-col gap-8 lg:w-[50%] text-white">
           <h1 className="text-3xl lg:text-5xl font-bold leading-[1.2]">
             People and Technology, Perfectly Aligned. The Future Is You.
           </h1>
@@ -43,19 +42,19 @@ export default function Home() {
             lives, and build a sustainable future.
           </p>
           <div className="flex flex-col md:flex-row gap-5 md:items-center">
-            <button className="btn bg-deep-blue text-accent-blue py-6">
+            <button className="btn bg-deep-blue text-accent-blue py-6 text-lg font-normal">
               Get Scholarship
             </button>
 
             <div className="flex gap-5 items-center">
               <span>Be Future-Ready</span>
-              <BiStar className="text-accent-blue" size={30}/>
+              <BiStar className="text-accent-blue" size={30} />
             </div>
           </div>
         </div>
 
         <div className="sticky top-0">
-          <div className="bg-deep-blue hidden md:flex justify-center gap-2 py-2 w-full">
+          <div className="bg-deep-blue hidden md:flex justify-center gap-2 py-2 w-full items-center text-white">
             <span className="text-xs">
               We empower individuals and organizations through cutting-edge tech
               training.
@@ -144,12 +143,15 @@ export default function Home() {
               Apply now
             </button>
 
-            <BiMenu size={40} className="md:hidden cursor-pointer hover:text-accent-blue"/>
+            <BiMenu
+              size={40}
+              className="md:hidden cursor-pointer hover:text-accent-blue"
+            />
           </nav>
         </div>
 
         <section className="hidden lg:flex absolute bottom-[-18px] left-1/4">
-          <div className="flex gap-10 justify-center items-center py-8">
+          <div className="flex gap-10 justify-center items-center py-8 text-white">
             <div className="flex flex-col gap- border-l-2 border-accent-blue px-2">
               <h2 className="text-lg font-medium">Discover 1411 </h2>
               <span className="text-sm">Innovative tech programs</span>
@@ -172,62 +174,123 @@ export default function Home() {
         </section>
       </header>
 
-      <main>
-        <section className="flex flex-col md:flex-row bg-white justify-center items-center px-6 lg:px-60 gap-4 overflow-hidden">
-          <div className="py-14 text-gray-400 text-sm md:text-xl md:text-right">
+      <main className="bg-white flex flex-col gap-10">
+        <section className="flex flex-col md:flex-row bg-white justify-center items-center gap-4 md:gap-10 overflow-hidden px-8 md:px-0 py-5">
+          <div className="text-gray-400 text-sm md:text-xl md:text-right w-full md:w-[400px]">
             Our trusted partners and clients, together, we drive success and
             make a lasting impact in the industry.
           </div>
 
-          <Marquee gradient={false} speed={50}>
-            <Image
-              src="/axa-logo.png"
-              alt="Axa"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
+          <div className="overflow-hidden relative w-full md:w-[400px] h-[120px]">
+            <div className="absolute right-0 flex space-x-1 slide">
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/axa-logo.png"
+                  alt="Axa"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
 
-            <Image
-              src="/vida.png"
-              alt="vida"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/vida.png"
+                  alt="vida"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
 
-            <Image
-              src="/vfd-tech.png"
-              alt="vfd"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/vfd-tech.png"
+                  alt="vfd"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
 
-            <Image
-              src="/coa.jpg"
-              alt="coa"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/coa.jpg"
+                  alt="coa"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
 
-            <Image
-              src="/anchor.png"
-              alt="anchor"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/anchor.png"
+                  alt="anchor"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
 
-            <Image
-              src="/dc.png"
-              alt="datacamp donates"
-              width={100}
-              height={100}
-              className="mx-4"
-            />
-          </Marquee>
+              <div className="w-[100px] h-[80px]">
+                <Image
+                  src="/dc.png"
+                  alt="datacamp donates"
+                  width={100}
+                  height={100}
+                  className="w-full h-full"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col w-full md:max-w-6xl mx-auto gap-20 px-8 md:px-0">
+          <div className="flex flex-col md:flex-row gap-10 items-center">
+            <div className="md:w-[500px] h-[300px] flex md:flex-1/2">
+              <Image
+                src="/marketing-team.jpg"
+                alt="marketing team"
+                width={500}
+                height={300}
+                className="object-cover shadow-2xl shadow-accent-blue rounded-t-[50px] rounded-bl-[50px]"
+              />
+            </div>
+
+            <div className="flex flex-col md:flex-1/2 gap-8">
+              <h1 className="font-bold text-xl md:text-3xl">
+                Tomorrow's Tech, Today's Innovation
+              </h1>
+
+              <p className="text-md md:text-lg">
+                Innovation is at the heart of what we do. We offer a range of
+                services to support startups and businesses, helping them to
+                launch, grow, and scale. Our solutions encompass tech training,
+                consulting, and comprehensive support in areas such as software
+                development, data analytics, business acceleration, digital
+                transformation, and more.
+              </p>
+
+              <div className="flex items-center space-x-5">
+                <Link
+                  href="#"
+                  className="font-normal text-teal-500 hover:text-black"
+                >
+                  Why Choose Us
+                </Link>
+                <BsArrowUpRight className="text-teal-500" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row items-center gap-4 md:gap-16 max-w-6xl mx-auto">
+            <span className="bg-deep-blue text-accent-blue rounded-[40px] p-3 text-2xl md:text-[70px] font-bold">
+              70% off
+            </span>
+
+            <h1 className="text-3xl md:text-[80px] font-bold">Scholarship</h1>
+          </div>
         </section>
       </main>
     </div>
